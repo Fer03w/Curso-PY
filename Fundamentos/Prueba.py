c05c14602e6dd9ed8a07193be1c619393df7a71f
Fernanda <ferhear60@gmail.com>
2022-02-05 17:24:54 -0500

    inicio

---
+++ Fundamentos/Prueba.py
@@ -0,0 +1,18 @@
+#Nuestro primer script
+'''
+A
+A
+A
+A
+Este es un comentario largo
+'''
+
+#Entrada y salida de datos se realiza con input
+#Salida de datos se realiza con print
+print('Formulario')
+print('ingresa tu nombre:')
+nombre = input()
+print('El nombre ingresado es: ',nombre)
+
+
+
